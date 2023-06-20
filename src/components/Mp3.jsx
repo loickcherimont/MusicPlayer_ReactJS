e3de82a2e673e84d4374a858960120c8ba271209
loickcherimont <loickcherimont@gmail.com>
2023-06-20 22:57:29 +0200

    Remove src attr. and audio tag to keep only song infos

--- src/components/Mp3.jsx
+++ src/components/Mp3.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
-export function Mp3({title, author, src}) {
-    return <div class="text-center flex flex-col items-center justify-center">
-
-        {/* <audio src={src} autoPlay loop>
-    <source src={src}/>
-</audio> */}
-    {/* Toggle the class .messagedefilant
-    to turn on/off title scrolling */}
-    <div class="messagedefilant">
-        <div class="font-bold text-white text-md"><span>{title}</span>
+export function Mp3({ title, author }) {
+    return <div className="text-center flex flex-col items-center justify-center">
+        <div className="messagedefilant">
+            <div className="font-bold text-white text-md"><span>{title}</span>
+            </div>
         </div>
-      </div>
-    <p class="text-mpLightGray text-xs">{author}</p>
-</div>
+        <p className="text-mpLightGray text-xs">{author}</p>
+    </div>
 }

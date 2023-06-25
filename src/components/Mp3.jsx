c8534d97cb696d835334371cf2e18eb097d396d9
loickcherimont <loickcherimont@gmail.com>
2023-06-25 22:22:37 +0200

    Add x-scrolling effect when music is playing

--- src/components/Mp3.jsx
+++ src/components/Mp3.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export function Mp3({ title, author }) {
+export function Mp3({ title, author, isPlaying }) {
     return <div className="text-center flex flex-col items-center justify-center">
-        <div className="messagedefilant">
+        <div className={isPlaying ? "messagedefilant":undefined}>
             <div className="font-bold text-white text-md"><span>{title}</span>
             </div>
         </div>

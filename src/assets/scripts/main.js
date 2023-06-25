32023c09435d3e758b4c2fb8234e8058cf527dd3
loickcherimont <loickcherimont@gmail.com>
2023-06-25 22:23:21 +0200

    Add a function that converts seconds in [minutes].[seconds]

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,3 @@
-
 export async function fetchTracks(path, options={}) {
     const headers = {
         'Content-Type': "application/json",
@@ -9,4 +8,14 @@ export async function fetchTracks(path, options={}) {
     if(r.ok) {
         return await r.json();
     }
+}
+
+// Fetch a duration in seconds
+// Returns an object representing minutes/seconds for a track
+export function getTrackTime(duration) {
+    const roundedDuration = Math.round(duration);
+    const seconds = roundedDuration % 60;
+
+    const minutes = (roundedDuration - seconds) / 60;
+    return `0${minutes}.${seconds < 10 ? `0${seconds}`:seconds}`;
 }
